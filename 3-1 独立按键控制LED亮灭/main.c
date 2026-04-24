4a2fe2c682172c6325d19aa19db980a4a3e5bd8b
hfhfff <dyftfboys@163.com>
2026-04-24 10:15:42 +0800

    if控制按键

--- 3-1 独立按键控制LED亮灭/main.c
+++ 3-1 独立按键控制LED亮灭/main.c
@@ -1,17 +1,46 @@
 #include <regx52.h>
 
+void Delay1ms(unsigned int xms)		//@12.000MHz
+{
+	unsigned char i, j;
+    while (xms)
+    {
+        i = 2;
+        j = 239;
+        do
+        {
+            while (--j);
+        } while (--i);
+        xms--;
+    }
+    
+}
 void main()
 {
+    int i = 0;
     while (1)
     {
-        if (P3_1 == 0)
+        if (i <= 7)
         {
-            P2_0 = 0;
+            if (P3_1 == 0)
+            {
+                P2 &= ~(1 << i);
+                
+            }
+            else
+            {
+                P2 |= ~(1 << i);//led off
+                
+            }
+            
+            
         }
         else
         {
-            P2_0 = 1;//led off
+            i = 0;
         }
+        
     }
-    
+    i = i + 1;
+        Delay1ms(1000);
 }
