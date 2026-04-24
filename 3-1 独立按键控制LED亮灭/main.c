646e8a9f541b6079529639f913b8d390622fc5f7
hfhfff <dyftfboys@163.com>
2026-04-24 09:44:40 +0800

    3-1 独立按键控制LED亮灭， K1控制LED一的亮灭，按下去不松手，保持亮一松手就熄灭了。K1对应单片机的io口—— P31

---
+++ 3-1 独立按键控制LED亮灭/main.c
@@ -0,0 +1,17 @@
+#include <regx52.h>
+
+void main()
+{
+    while (1)
+    {
+        if (P3_1 == 0)
+        {
+            P2_0 = 0;
+        }
+        else
+        {
+            P2_0 = 1;//led off
+        }
+    }
+    
+}
