d9e51c3b8a2297b00deb5bf9ccf0c91c41fac11b
hfhfff <dyftfboys@163.com>
2026-04-25 14:12:43 +0800

    每900ms加1

--- 5-2 LCD1602调试工具/mian.c
+++ 5-2 LCD1602调试工具/mian.c
@@ -1,19 +1,21 @@
 #include <regx52.h>
 #include "LCD1602.h"
+#include "delay.h"
 
+int Result=0;
 void main()
 {
     LCD_Init();
-    LCD_ShowChar(1, 1, 'A');
-    LCD_ShowString(1, 3, "LV LOVE YU");
-    LCD_ShowNum(1, 14, 123, 3);
-    LCD_ShowNum(2, 1, 2, 1);
-    LCD_ShowSignedNum(2, 2, -5, 1);
-    LCD_ShowString(2, 4, "=-3");
-    LCD_ShowHexNum(2, 8, 0xff, 2);
-    LCD_ShowBinNum(2, 11, 0x0C, 6);
+    
     while (1)
     {
-        
+        if (Result==15)
+        {
+            Result = 0;
+        }
+				
+        LCD_ShowSignedNum(1, 1, Result, 3);
+        Result++;
+        Delay(900);
     }
 }
