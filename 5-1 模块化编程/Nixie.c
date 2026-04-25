d1c0e9e11aa777eceed9b0336a5e8e621a1d2d00
hfhfff <dyftfboys@163.com>
2026-04-25 10:43:12 +0800

    Nixie函数也成功模块化，实现了数码管的动态显示

---
+++ 5-1 模块化编程/Nixie.c
@@ -0,0 +1,21 @@
+#include <regx52.h>
+#include "Delay.h"
+
+unsigned char NixieNumber[] = { 0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f };
+
+void Nixie(unsigned char Location, Number)
+    {
+        switch (Location)
+        {
+            case 1:P2_4 = 1;P2_3 = 1;P2_2 = 1;break;//实际是led7，但是从左往右第8,人为把他记为第一个，因为顺眼
+            case 2:P2_4 = 1;P2_3 = 1;P2_2 = 0;break;//实际是led6，但是从左往右第7
+            case 3:P2_4 = 1;P2_3 = 0;P2_2 = 1;break;//实际是led5，但是从左往右第6
+            case 4:P2_4 = 1;P2_3 = 0;P2_2 = 0;break;//实际是led4，但是从左往右第5
+            case 5:P2_4 = 0;P2_3 = 1;P2_2 = 1;break;//实际是led3，但是从左往右第4
+            case 6:P2_4 = 0;P2_3 = 1;P2_2 = 0;break;//实际是led2，但是从左往右第3
+            case 7:P2_4 = 0;P2_3 = 0;P2_2 = 1;break;//实际是led1，但是从左往右第2
+            case 8:P2_4 = 0;P2_3 = 0;P2_2 = 0;break;//实际是led0，但是从左往右第1
+            default:break;
+        }
+        P0 = NixieNumber[Number];
+    }
--- 5-1 模块化编程/main.c
+++ 5-1 模块化编程/main.c
@@ -1,13 +1,30 @@
 #include <regx52.h>
 #include "Delay.h"
+#include "Nixie.h"
 
 void main()
 {
     while (1)
     {
-        Delay(500);
-        P2=0xfe;
-        Delay(700);
-        P2=0xff;
+        // Delay(500);
+        // P2 = 0xfe;
+        // Delay(700);
+        // P2 = 0xff;
+        Nixie(1, 1);
+        Delay(1);
+        Nixie(2, 2);
+        Delay(1);
+        Nixie(3, 3);
+        Delay(1);
+        Nixie(4, 4);
+        Delay(1);
+        Nixie(5, 5);
+        Delay(1);
+        Nixie(6, 6);
+        Delay(1);
+        Nixie(7, 7);
+        Delay(1);
+        Nixie(8, 8);
+        Delay(1);
     }
 }
