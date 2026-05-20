46953b05f9d0dc0ab0aea8ce9321e46eed0af012
hfhfff <dyftfboys@163.com>
2026-05-20 17:19:11 +0800

    好像不对256取余也是正常的结果

--- 12-1 AT24C02 数据存储/main.c
+++ 12-1 AT24C02 数据存储/main.c
@@ -4,19 +4,36 @@
 #include "key.h"
 #include "AT24C02.h"
 
-unsigned char Data;
+unsigned char KeyNum;
+signed char  Count, Data;
 
 void main()
 {
     LCD_Init();
-    LCD_ShowString(1, 1, "Hello");
-    AT24C02_WriteByte(1, 66);
-    Delay(7);
-    Data = AT24C02_RandRead(1);
-    LCD_ShowNum(2, 1, Data, 3);
-
     while (1)
     { 
-        
+        KeyNum = Key();
+        switch (KeyNum)
+        {
+        case 1:{LCD_Init();Count++; LCD_ShowSignedNum(1, 1, Count, 3);}break;
+        case 2:{LCD_Init();Count--; LCD_ShowSignedNum(1, 1, Count, 3);}break;
+        case 3:
+        {
+            AT24C02_WriteByte(0, Count);
+            Delay(5);
+            // AT24C02_WriteByte(1, Count % 256);
+            // Delay(5);
+            LCD_ShowSignedNum(1, 1, Count, 3);
+            LCD_ShowString(1, 10, "Write");
+        }break;
+        case 4:
+        {
+            Data = AT24C02_RandRead(0);
+                // * 10 + AT24C02_RandRead(1);
+            LCD_ShowSignedNum(1, 1, Data, 3);
+            LCD_ShowString(1, 10, "read");
+        }break;
+        default:break;
+        }
     }
 }
