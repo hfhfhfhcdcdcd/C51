5ad941ccf6da179f779be446b9856afa0928ad22
hfhfff <dyftfboys@163.com>
2026-05-22 11:57:59 +0800

    got it

--- 12-2 秒表(定时器扫描按键数码管)/main.c
+++ 12-2 秒表(定时器扫描按键数码管)/main.c
@@ -7,7 +7,33 @@
 #include "Nixie.h"
 
 #define DELAY_MS 2
-unsigned char KeyNum,Temp;
+unsigned char KeyNum, Temp;
+unsigned char En;
+unsigned char Count100, Count_s, Count_min;
+
+
+void CountClock(void)
+{
+    if (En == 1)
+    {
+        Count100++;
+        if (Count100 == 100)
+        {
+            Count100 = 0;
+            Count_s++;
+            if (Count_s == 60)
+            {
+                Count_s = 0;
+                Count_min++;
+                if (Count_min == 60)
+                {
+                    Count_min = 0;
+                }
+            }
+        }
+
+    }
+}
 
 void main()
 {
@@ -15,19 +41,47 @@ void main()
     while (1)
     {
         KeyNum = Key();
-        Nixie_Set(1, 0);
-        Nixie_Set(2, 0);
-        Nixie_Set(3, 10);
-        Nixie_Set(4, 0);
-        Nixie_Set(5, 0);
-        Nixie_Set(6, 10);
-        Nixie_Set(7, 0);
-        Nixie_Set(8, 0);
         if (KeyNum)
         {
-            
-            Nixie_Set(3, KeyNum);
+            switch (KeyNum)
+            {
+                case 1: En = !En;break;
+                case 2:
+                {
+                    En = 0;
+                    Count100 = 0;
+                    Count_s = 0;
+                    Count_min = 0;
+                }break;
+
+                case 3:
+                {
+                    AT24C02_WriteByte(0, Count_min);//分
+                    Delay(5);
+                    AT24C02_WriteByte(1, Count_s);//秒
+                    Delay(5);
+                    AT24C02_WriteByte(2, Count100);//ms
+                    Delay(5);
+                }break;
+                
+                case 4:
+                {
+                    En = 0;
+                    Count_min = AT24C02_RandRead(0);
+                    Count_s = AT24C02_RandRead(1);
+                    Count100 = AT24C02_RandRead(2);
+                }    break;
+                default:break;
+            }
         }
+        Nixie_Set(1, Count_min / 10);
+        Nixie_Set(2, Count_min % 10);
+        Nixie_Set(3, 10);
+        Nixie_Set(4, Count_s / 10);
+        Nixie_Set(5, Count_s % 10);
+        Nixie_Set(6, 10);
+        Nixie_Set(7, Count100 / 10);
+        Nixie_Set(8, Count100 % 10);
 
     }
     
@@ -38,7 +92,7 @@ void main()
 
 void Timer0_Rountine() interrupt 1
 {
-    static unsigned int T0Count1,T0Count2;
+    static unsigned int T0Count1,T0Count2,T0Count3;
     TH0 = 0xFC;
     TL0 = 0x18;
     T0Count1 ++;
@@ -53,5 +107,11 @@ void Timer0_Rountine() interrupt 1
     {
         T0Count2 = 0;
         Nixie_Loop();
+    }
+    T0Count3++;
+    if (T0Count3 == 10)
+    {
+        T0Count3 = 0;
+        CountClock();
     }
 }
