2fcf383ef30384239adf82a3563bf158d9f9c8bf
hfhfff <dyftfboys@163.com>
2026-05-23 23:56:47 +0800

    改正版，错的原因写在了txt中

--- 13-1 DS18B20温度读取/Onewire.c
+++ 13-1 DS18B20温度读取/Onewire.c
@@ -1,86 +1,71 @@
-#include <REGX52.H>
+#include "REGX52.H"
+#include "Delay.h"
+#include <intrins.h>  
 
-//引脚定义
-sbit OneWire_DQ=P3^7;
+sbit OneWire_DQ = P3 ^ 7;
 
-/**
-  * @brief  单总线初始化
-  * @param  无
-  * @retval 从机响应位，0为响应，1为未响应
-  */
-unsigned char OneWire_Init(void)
+unsigned char Onewire_Init(void)
 {
-	unsigned char i;
-	unsigned char AckBit;
-	OneWire_DQ=1;
-	OneWire_DQ=0;
-	i = 247;while (--i);		//Delay 500us
-	OneWire_DQ=1;
-	i = 32;while (--i);			//Delay 70us
-	AckBit=OneWire_DQ;
-	i = 247;while (--i);		//Delay 500us
-	return AckBit;
+    bit AckBit;
+    OneWire_DQ = 1;
+    OneWire_DQ = 0;
+    Delay480us();
+    OneWire_DQ = 1;
+    Delay80us();
+    AckBit = OneWire_DQ;
+    OneWire_DQ = 1;
+    Delay480us();
+
+    return AckBit;
 }
 
-/**
-  * @brief  单总线发送一位
-  * @param  Bit 要发送的位
-  * @retval 无
-  */
-void OneWire_SendBit(unsigned char Bit)
+void OneWire_SendBit(bit Bit)
 {
-	unsigned char i;
-	OneWire_DQ=0;
-	i = 4;while (--i);			//Delay 10us
-	OneWire_DQ=Bit;
-	i = 24;while (--i);			//Delay 50us
-	OneWire_DQ=1;
+    OneWire_DQ = 0;
+    Delay10us();
+    OneWire_DQ = Bit;
+    if (Bit)
+    {
+        OneWire_DQ = 1;
+        Delay50us();
+    }
+    else
+    {
+        Delay50us();
+        OneWire_DQ = 1;
+    }
 }
 
-/**
-  * @brief  单总线接收一位
-  * @param  无
-  * @retval 读取的位
-  */
-unsigned char OneWire_ReceiveBit(void)
+bit OneWire_ReceiveBit(void)
 {
-	unsigned char i;
-	unsigned char Bit;
-	OneWire_DQ=0;
-	i = 2;while (--i);			//Delay 5us
-	OneWire_DQ=1;
-	i = 2;while (--i);			//Delay 5us
-	Bit=OneWire_DQ;
-	i = 24;while (--i);			//Delay 50us
-	return Bit;
+	bit Bit;
+	OneWire_DQ = 0;
+	Delay10us();         // 主机拉低10us
+	OneWire_DQ = 1;
+	Bit = OneWire_DQ;    // 读取
+	
+	Delay50us();
 }
 
-/**
-  * @brief  单总线发送一个字节
-  * @param  Byte 要发送的字节
-  * @retval 无
-  */
 void OneWire_SendByte(unsigned char Byte)
 {
-	unsigned char i;
-	for(i=0;i<8;i++)
-	{
-		OneWire_SendBit(Byte&(0x01<<i));
-	}
+    unsigned char i;
+    for (i = 0;i < 8;i++)
+    {
+        OneWire_SendBit(Byte & (0x01 << i));
+    }
 }
 
-/**
-  * @brief  单总线接收一个字节
-  * @param  无
-  * @retval 接收的一个字节
-  */
 unsigned char OneWire_ReceiveByte(void)
 {
-	unsigned char i;
-	unsigned char Byte=0x00;
-	for(i=0;i<8;i++)
-	{
-		if(OneWire_ReceiveBit()){Byte|=(0x01<<i);}
-	}
-	return Byte;
+    unsigned char i;
+    unsigned char Byte = 0x00;
+    for (i = 0;i < 8;i++)
+    {
+        if(OneWire_ReceiveBit())
+        {
+            Byte |= (0x01 << i);
+        }
+    }
+    return Byte;
 }
--- 13-1 DS18B20温度读取/main.c
+++ 13-1 DS18B20温度读取/main.c
@@ -2,30 +2,19 @@
 #include "delay.h"
 #include "LCD1602.h"
 #include "DS18B20.h"
+#include <STDIO.H> 
 
-float T;
+float Temperature;
 void main()
 {
-	// DS18B20_Convert();
-	T = DS18B20_GetTemp();
-	Delay(1000);
-    LCD_Init();
+	// Temperature = DS18B20_GetTemp();
+	LCD_Init();
     LCD_ShowString(1, 1, "Temperature:");
     while (1)
     {
-        // DS18B20_Convert();
-		T = DS18B20_GetTemp();
-        if(T<0)				//如果温度小于0
-		{
-			LCD_ShowChar(2,1,'-');	//显示负号
-			T=-T;			//将温度变为正数
-		}
-		else				//如果温度大于等于0
-		{
-			LCD_ShowChar(2,1,'+');	//显示正号
-		}
-		LCD_ShowNum(2,2,T,3);		//显示温度整数部分
-		LCD_ShowChar(2,5,'.');		//显示小数点
-		LCD_ShowNum(2,6,(unsigned long)(T*10000)%10000,4);//显示温度小数部分
+        Temperature = DS18B20_GetTemp();
+        LCD_ShowSignedNum(2,1,Temperature,2);		//显示温度整数部分
+		LCD_ShowChar(2,4,'.');		//显示小数点
+		LCD_ShowNum(2,5,(unsigned long)(Temperature*10000)%10000,4);
     }
 }
