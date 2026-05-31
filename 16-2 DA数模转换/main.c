8814ef4c42c10bd3d6c6bb20762670a1254ab27b
hfhfff <dyftfboys@163.com>
2026-05-31 16:21:22 +0800

    DA实现led呼吸灯

--- 16-2 DA数模转换/main.c
+++ 16-2 DA数模转换/main.c
@@ -4,8 +4,8 @@
 #include "Nixie.h"
 #include "Timer0.h"
 
-sbit Motor = P1 ^ 0;
-unsigned char Counter, Compare,KeyNum,Speed; //计数器和比较值
+sbit DA = P2 ^ 1;
+unsigned char Counter, Compare,i; //计数器和比较值
 
 void main()
 {
@@ -13,32 +13,32 @@ void main()
     Compare = 5; //占空比
     while (1)
     {
-        KeyNum = Key();
-        if (KeyNum == 1)
+        for (i = 0;i < 100;i++)
         {
-            Speed++;//速度加
-            Speed %= 4;
-            if (Speed == 0) { Compare = 0; }
-            if (Speed == 1) { Compare = 55; }
-            if (Speed == 2) { Compare = 80; }
-            if (Speed == 3) { Compare = 100; }
+            Compare = i;
+            Delay(5);
         }
-            Nixie_Scan(1, Speed);
+        for (i = 100;i > 0;i--)
+        {
+            Compare = i;
+            Delay(5);
+        }
+
     }
 }
 
 void Timer0_Rountine() interrupt 1
 {
-    TH0 = 0xFF;
+    TH0 = 0xFF;//1us来一次中断
     TL0 = 0x9C;
     Counter++;
     Counter %= 100;
     if (Counter < Compare)
     {
-        Motor = 1;
+        DA = 0;
     }
     else
     {
-        Motor = 0;
+        DA = 1;
     }
 }
